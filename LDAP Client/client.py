4d0a658373e02d705b790cdf8ff773efb08bc1ed
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-06-05 13:18:51 +0000

    LDAP Client Implementation

---
+++ LDAP Client/client.py
@@ -0,0 +1,21 @@
+import socket
+
+# Connect to the LDAP server
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 389))
+
+# Construct the LDAP search request
+request = ...
+
+# Send the request
+sock.sendall(request)
+
+# Receive the response
+response = sock.recv(4096)
+
+# Parse the response
+entries = ...
+
+# Print the entries
+for entry in entries:
+    print(entry)
