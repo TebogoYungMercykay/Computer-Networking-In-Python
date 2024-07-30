7d1ed08639dd1e34876e4d5356959c575ed37fe4
tebogoyungmercykay <u20748052@tuks.co.za>
2024-07-30 11:33:40 +0200

    (formatter) POP3 Client and Proxy

--- POP3 Client+Proxy/clean_data.py
+++ POP3 Client+Proxy/clean_data.py
@@ -1,10 +1,12 @@
 import base64
 
+
 def extract_recipient(packet):
     to_index = packet.find("To: ") + len("To: ")
     newline_index = packet.find("\n", to_index)
     recipient = packet[to_index:newline_index].strip()
     return recipient
+
 
 def extract_message(packet):
     to_index = packet.find("To: ") + len("To: ")
@@ -12,17 +14,19 @@ def extract_message(packet):
     recipient = packet[newline_index:].strip()
     return recipient
 
+
 def base64_decode_bytes(encoded_bytes):
     decoded_bytes = base64.b64decode(encoded_bytes)
-    decoded_str = decoded_bytes.decode('UTF-8')
+    decoded_str = decoded_bytes.decode("UTF-8")
     return decoded_str
+
 
 def clean_data(packet, mimetype):
     recipient = extract_recipient(packet)
-    message = extract_message(packet) # returns a string
-    if mimetype == 'application/octet-stream':
+    message = extract_message(packet)  # returns a string
+    if mimetype == "application/octet-stream":
         print("base64 mimetype identified")
-        message = message.encode('UTF-8')
+        message = message.encode("UTF-8")
         message = base64_decode_bytes(message)
     print("[MailServer] Message: ", message)
     if "Confidential" in message:
